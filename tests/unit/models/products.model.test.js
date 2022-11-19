11c0597cdfd68ed85de81866f98c792724645ef6
Pedro Luiz <pedroluizlsg@gmail.com>
2022-11-19 18:58:00 -0300

    Req 2 -  em andamento - teste product model

---
+++ tests/unit/models/products.model.test.js
@@ -0,0 +1,40 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const { expect } = chai;
+
+const productsModel = require('../../../src/models/products.model');
+const connection = require('../../../src/models/db/connection');
+const mock = require('../models/mock/products.mock');
+
+describe('Teste da camada model - rota /products', function () {
+
+  afterEach(function () { sinon.restore() });
+
+  describe('Teste da função getAll', function () {
+    it('retorna todos os produtos cadastrados', async function () {
+      // Arrange -> configurações do teste
+      sinon.stub(connection, 'execute').resolves([mock.productsDb]);
+
+      // Act -> camada da função
+      const response = await productsModel.getAll();
+
+      // Assert -> o que esperamos de retorno da função
+      expect(response).to.be.equal(mock.productsDb);
+    });
+  });
+
+  describe('Teste da função getById', function () {
+    it('retorna o produto cadastrado com o respectivo Id', async function () {
+      const id = 1;
+      // Arrange -> configurações do teste
+      sinon.stub(connection, 'execute').resolves([mock.productsDb[0]]);
+
+      // Act -> camada da função
+      const response = await productsModel.getById(id);
+
+      // Assert -> o que esperamos de retorno da função
+      expect(response).to.be.equal(mock.productsDb[0]);
+    });
+  });
+
+});
